efc894f7564ff912662acb704e54fbd97efee9e3
DRohan007 <drohan940@gmail.com>
2020-10-04 12:34:59 +0000

    sanders: Use BOARD_VNDK_VERSION := current  *Remove vndk-sp  *BOARD_VNDK_RUNTIME_DISABLE := true
    Signed-off-by: DRohan007 <drohan940@gmail.com>
    Signed-off-by: ashwatthama <sai404142@gmail.com>

--- device.mk
+++ device.mk
@@ -459,8 +459,8 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service
 
 # VNDK
-PRODUCT_PACKAGES += \
-    vndk-sp
+#PRODUCT_PACKAGES += \
+#    vndk-sp
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
